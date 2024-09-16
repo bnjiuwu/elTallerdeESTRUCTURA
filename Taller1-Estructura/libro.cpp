5c00fb36f039a167de7563e91a397d5fae49ac48
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-09-16 18:14:08 -0300

    Update libro.cpp

--- Taller1-Estructura/libro.cpp
+++ Taller1-Estructura/libro.cpp
@@ -1,7 +1,8 @@
 #include "libro.h"
+#include "MaterialBibliografico.cpp"
 #include <string>
 
-libro::libro(string Nombre, string Autor, string ISBN, bool Prestado, string FechaDePublicacion): MaterialBibliografico(string Nombre, string Autor, string ISBN, bool Prestado)
+libro::libro(string Nombre, string Autor, string ISBN, bool Prestado, string FechaDePublicacion): MaterialBibliografico(Nombre, Autor, ISBN, Prestado)
 {
 
   this-> FechaDePublicacion = FechaDePublicacion;
