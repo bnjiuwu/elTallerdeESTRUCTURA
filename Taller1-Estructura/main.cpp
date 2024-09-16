63866d65138275b201b2dbd1e4770a3fe6367473
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-09-16 18:13:36 -0300

    Update main.cpp

--- Taller1-Estructura/main.cpp
+++ Taller1-Estructura/main.cpp
@@ -1,7 +1,8 @@
-#include "MaterialBibliografico.cpp"
 #include "libro.cpp"
 
 int main(){
     libro* material = new libro("Principito","pepe","Vision",true, "10 de Octubre");
-    material->~libro();
-};
+    material->Mostrarinfo();
+    material ->~libro();
+    return 0;
+}
