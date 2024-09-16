a1de96657da1faff85859b4664838c4c8192c9d7
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-09-16 17:50:57 -0300

    Create MaterialBibliografico.cpp

---
+++ Taller1-Estructura/MaterialBibliografico.cpp
@@ -0,0 +1,37 @@
+#include "MaterialBibliografico.h"
+
+MaterialBibliografico::MaterialBibliografico(string Nombre, string Autor, string ISBN, bool Presstado)
+{
+    this ->Nombre = Nombre;
+    this ->Autor = Autor;
+    this ->ISBN = ISBN;
+    this ->Prestado = Prestado;
+}
+
+MaterialBibliografico::~MaterialBibliografico()
+{
+    std::cout<<"Se ha eliminado "<<std::endl;
+}
+
+string MaterialBibliografico::getNombre()
+{
+    return this->Nombre;
+}
+
+int MaterialBibliografico::Mostrarinfo()
+{
+    cout<<"Nombre del Libro: "<<this->Nombre<<std::endl;
+    cout<<"Autor del Libro: "<<this-> Autor<<std::endl;
+    cout<<"ISBN del Libro: "<<this->ISBN<<std::endl;
+    string estado = "";
+    if(this->Prestado == true)
+    {
+        estado = "Prestado";
+    }
+    else
+    {
+        estado = "No Prestado";
+    }
+    cout<<"Estado del Libro: "<<estado<<std::endl;
+    return 0;
+}
