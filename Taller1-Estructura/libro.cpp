b3c4407ef2918b826e709cb2c1a3150a7e4661b7
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-09-16 17:50:20 -0300

    Update libro.cpp

--- Taller1-Estructura/libro.cpp
+++ Taller1-Estructura/libro.cpp
@@ -1,11 +1,14 @@
-#pragma once
 #include "libro.h"
+#include <string>
 
+libro::libro(string Nombre, string Autor, string ISBN, bool Prestado, string FechaDePublicacion): MaterialBibliografico(string Nombre, string Autor, string ISBN, bool Prestado)
+{
 
-class libro{
+  this-> FechaDePublicacion = FechaDePublicacion;
+  
+}
 
-  private:
-    
-
-
-};
+libro::~libro()
+{
+  cout<<"Se ha eliminado el libro: "<< this-> Nombre<<std::endl;
+}
