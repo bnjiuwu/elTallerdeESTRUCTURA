f8959dbc349e9c4f81c5ea14507e68a759a536e2
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-09-16 17:49:51 -0300

    Update libro.h

--- Taller1-Estructura/libro.h
+++ Taller1-Estructura/libro.h
@@ -1,1 +1,16 @@
+#pragma once
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class libro: public MaterialBibliografico
+{
+    protected:
+    string FechaDePublicacion;
+
+    public:
+    libro(string Nombre, string Autor, string ISBN, bool Prestado, string FechaDePublicacion);
+    ~libro();
+
+};
