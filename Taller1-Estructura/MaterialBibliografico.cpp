d219618b188896c9bb4684ce7f1938c656e4a8f9
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-09-17 13:18:01 -0300

    Update MaterialBibliografico.cpp Eneo

--- Taller1-Estructura/MaterialBibliografico.cpp
+++ Taller1-Estructura/MaterialBibliografico.cpp
@@ -13,17 +13,22 @@ MaterialBibliografico::~MaterialBibliografico()
     std::cout<<"Se ha eliminado la clase padre"<<std::endl;
 }
 
-string MaterialBibliografico::getNombre()
+void MaterialBibliografico::setEstado(string Estado)
 {
-    return this->Nombre;
+    this->Prestado = Estado;
 }
 
-int MaterialBibliografico::Mostrarinfo()
+string MaterialBibliografico::getEstado()
+{
+    return this->Prestado;
+}
+
+void MaterialBibliografico::Mostrarinfo()
 {
     cout<<"Nombre del Libro: "<<this->Nombre<<std::endl;
     cout<<"Autor del Libro: "<<this-> Autor<<std::endl;
     cout<<"ISBN del Libro: "<<this->ISBN<<std::endl;
     cout<<"Estado del Libro: "<<this->Prestado<<std::endl;
-    return 0;
+    return;
 }
 
