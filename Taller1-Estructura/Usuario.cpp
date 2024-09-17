f1aac7b9e66f50e52afc6149995b6f7dbddecc5c
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-09-17 13:20:30 -0300

    Update Usuario.cpp Eneo

--- Taller1-Estructura/Usuario.cpp
+++ Taller1-Estructura/Usuario.cpp
@@ -8,19 +8,27 @@ Usuario::Usuario(string Nombre, string id)
 
 void Usuario::prestarMaterial(MaterialBibliografico* material)
 {
-    
-    MaterialBibliografico* aux = this->MaterialesPrestados[0] ;
-    while(aux == nullptr)
+    cout<<" "<<std::endl;
+    int aux = 0;
+    while(aux<5)
     {
-        if(aux == nullptr)
+        if(this->MaterialesPrestados[aux] == 0 && material->getEstado() == "No Prestado")
         {
-            aux = material;
-            
+            cout<<"Se ha guardado el Material"<<std::endl;
+            this->MaterialesPrestados[aux] = material;
+            material->setEstado("Prestado");
+            cout<<" "<<std::endl;
+            return;
         }
-        aux += 1;
+        else if(material->getEstado() == "Prestado")
+        {
+            cout<<"El material ya fue prestado"<<std::endl;
+            return;
+        }
+        aux++;
     }
-
-    cout<<"La lista de Material Bibliografico esta ocupado"<<std::endl;
+    cout<<"La lista esta completa, no se puede prestar mas Materiales"<<std::endl;
+    cout<<" "<<std::endl;
 }
 
 void Usuario::devolverMaterial(MaterialBibliografico* material)
@@ -30,6 +38,24 @@ void Usuario::devolverMaterial(MaterialBibliografico* material)
 
 void Usuario::mostrarMaterialesPrestados()
 {
+    cout<<" "<<std::endl;
+    int aux = 0;
+    int contador = 0;
+    while(aux<5)
+    {
+        if(this->MaterialesPrestados[aux] != nullptr)
+        {
+            MaterialBibliografico* objeto = this->MaterialesPrestados[aux];
+            objeto->Mostrarinfo();
+            contador += 1;
+        }
+        aux += 1;
+    }  
+    if(contador == 0)
+    {
+        cout<<"No hay libros en la lista"<<std::endl;  
+    }
+    cout<<" "<<std::endl;
 }
 
 Usuario::~Usuario()
