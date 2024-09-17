afd9cb612f39e08fa85f7ed27eff441282aaceb9
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-09-16 21:42:41 -0300

    Create Usuario.h

---
+++ Taller1-Estructura/Usuario.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "MaterialBibliografico.h"
+
+using namespace std;
+
+class Usuario
+{
+    private:
+    string Nombre;
+    string id;
+    MaterialBibliografico* MaterialesPrestados[5];
+
+    public:
+    Usuario(string Nombre, string id);
+    void prestarMaterial(MaterialBibliografico* material);
+    void devolverMaterial(MaterialBibliografico* material);
+    void mostrarMaterialesPrestados();
+    ~Usuario();
+
+
+};
