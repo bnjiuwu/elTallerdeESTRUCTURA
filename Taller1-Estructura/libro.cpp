26558b6df4dc1d9e565d527a72562ed146b3124b
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2024-09-19 12:43:14 -0300

    Update libro.cpp Eneo

--- Taller1-Estructura/libro.cpp
+++ Taller1-Estructura/libro.cpp
@@ -1,8 +1,8 @@
 #include "libro.h"
-#include "MaterialBibliografico.cpp"
+
 #include <string>
 
-libro::libro(string Nombre, string Autor, string ISBN, bool Prestado, string FechaDePublicacion, string resumen) : MaterialBibliografico(Nombre, Autor, ISBN, Prestado)
+libro::libro(string Nombre, string Autor, string ISBN, string Prestado, string FechaDePublicacion, string resumen) : MaterialBibliografico(Nombre, Autor, ISBN, Prestado)
 {
 
   this -> FechaDePublicacion = FechaDePublicacion;
@@ -13,4 +13,15 @@ libro::libro(string Nombre, string Autor, string ISBN, bool Prestado, string Fec
 libro::~libro()
 {
   cout<<"Se ha eliminado el libro: "<< this-> Nombre<<std::endl;
+}
+
+void libro::MostrarInfo() const
+{
+  cout<<"Nombre del Libro: "<<this->Nombre<<", ";
+  cout<<"Autor del Libro: "<<this-> Autor<<", ";
+  cout<<"ISBN del Libro: "<<this->ISBN<<", ";
+  cout<<"Estado del Libro: "<<this->Prestado<<", ";
+  cout<<"Fecha de Publicacion del Libro: "<<this->FechaDePublicacion<<", ";
+  cout<<"Resumen del Libro: "<<this->resumen<<", ";
+  cout<<" "<<std::endl;
 }
